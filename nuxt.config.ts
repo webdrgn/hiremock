81b01e158d97f4dad6ab2aedf89f28464bf8de6d
webdragon <grmnche@yandex.ru>
2026-06-16 15:15:02 +0300

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Lara from '@primeuix/themes/lara';
 
+const productionBaseUrl = '/pixel-resume/';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   app: {
+    baseURL: productionBaseUrl,
     pageTransition: { name: 'page', mode: 'out-in' },
   },
   modules: [
